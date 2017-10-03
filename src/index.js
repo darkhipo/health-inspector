bf661ee951feb0d915b10dff7c341358ccf241ad
Dmitri Arkhipov <darkhipo@gmail.com>
2017-10-02 20:58:57 -0700

    Inspector

--- src/index.js
+++ src/index.js
@@ -3,4 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const App = () => {
+    return (
+        <div>
+            <h1>Hello there World!</h1>
+            <p> I am the very model of a modern major general.</p>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
 registerServiceWorker();
